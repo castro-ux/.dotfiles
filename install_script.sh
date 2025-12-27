c85a015f5746d20e2a74194a92afd7deaa68a9b2
Leo C <131409350+castro-ux@users.noreply.github.com>
2025-12-27 14:05:36 -0500

    Remove AUR installation commands from script
    Removed AUR installation commands for yay and brave.

--- install_script.sh
+++ install_script.sh
@@ -22,10 +22,6 @@ PACKAGES=(
     tree gimp qutebrowser zsh unzip 
     dolphin dmenu
     )
-    
-# --- AUR TO INSTALL ---
-sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay-bin.git && cd yay-bin && makepkg -si
-yay -S --noconfirm brave-bin
 
 # --- CHECK AND INSTALL PACKAGES ---
 packages_to_install=()
